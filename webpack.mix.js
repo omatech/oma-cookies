07dd24b82e293845d13db9e3f5ca861dc18ad890
Saray Juárez <sjuarez@omatech.com>
2022-03-22 09:43:04 +0100

    Change icon font for svg cookie image and css x

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,11 @@
 const mix = require('laravel-mix');
 
 mix.copy('src/scss', 'dist/scss');
-mix.copy('src/fonts', 'dist/fonts');
 
 mix.sass('src/scss/omacookies.scss', 'css/omacookies.css');
 mix.minify('dist/css/omacookies.css');
 mix.js('src/js/main.js', 'dist/js/omacookies.js');
 mix.minify('dist/js/omacookies.js', "", true);
-mix.setResourceRoot('..');
 mix.setPublicPath('dist');
 
 mix.babelConfig({
